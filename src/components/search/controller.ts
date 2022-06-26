00324fb78d1315c65356dfe34e423590357b6b5b
Luis Criollos <luisecriollos@gmail.com>
2022-06-26 19:46:44 -0400

    se populan campos

--- src/components/search/controller.ts
+++ src/components/search/controller.ts
@@ -3,9 +3,16 @@ import store from '../../store/mongo';
 
 export default {
   async searchTerm(term: string) {
+    const fieldsToPopulate = '_id name username profileImg';
     try {
       const users = await store.list(E_TABLES.USER, { search: { field: 'name', term } });
-      const tweets = await store.list(E_TABLES.TWEETS, { search: { field: 'content', term } });
+      const tweets = await store.list(E_TABLES.TWEETS, {
+        populate: [
+          { field: 'likes', select: fieldsToPopulate },
+          { field: 'owner', select: fieldsToPopulate },
+        ],
+        search: { field: 'content', term },
+      });
       return { users, tweets };
     } catch (error) {
       throw error;
