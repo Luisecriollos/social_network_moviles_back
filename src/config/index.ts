52ed095da5bbf330e45dd6a44e0dc3cdf7e86af5
Luis Criollos <luisecriollos@gmail.com>
2022-07-06 15:39:43 -0400

    Revert "Se remueve firebase"
    This reverts commit b1716a2073984efd36b7d94fafe6a86867f966f9.

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,4 +6,13 @@ export default {
   jwt: {
     secret: process.env.JWT_SECRET || 'secret',
   },
+  firebase: {
+    apiKey: 'AIzaSyD8sf1-cqMxLWo7gDU0uIeQqiRn8_VNZEE',
+    authDomain: 'social-network-fs.firebaseapp.com',
+    projectId: 'social-network-fs',
+    storageBucket: 'social-network-fs.appspot.com',
+    messagingSenderId: '374203391919',
+    appId: '1:374203391919:web:fa34d4a170b8fcf1b9595a',
+    measurementId: 'G-GCCQSWB6RL',
+  },
 };
